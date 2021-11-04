ffbabd4bd223ce9c4438972c075e1331c6692b7a
Tiago Assunção <tiagoassunc@gmail.com>
2021-11-04 19:21:44 -0300

    :lipstick: Build theme foundations and applying it

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,10 @@
+import { ThemeProvider } from "theme-ui";
+import theme from "../src/theme/theme";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
---
+++ src/theme/theme.js
@@ -0,0 +1,9 @@
+import { tailwind } from "@theme-ui/presets";
+
+const theme = {
+ ...tailwind
+};
+
+console.log(theme)
+
+export default theme
