ad37d82d1bd80593fdd58ec880936022d6a7690f
Tiago Assunção <tiagoassunc@gmail.com>
2021-11-04 23:51:47 -0300

    :sparkles: Adding NavHeader component and background globally

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,15 @@
 import { ThemeProvider } from "theme-ui";
 import theme from "../src/theme/theme";
+import { global } from "../src/sass/global.scss"
+import NavHeader from '../src/components/navigation/NavHeader'
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+    <ThemeProvider theme={theme} className={global}>
+      <div>
+        <NavHeader />
+        <Component {...pageProps} />
+      </div>
     </ThemeProvider>
   );
 }
