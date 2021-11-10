700b61413b601ff14626343f4e122df2020ae520
Tiago Assunção <tiagoassunc@gmail.com>
2021-11-10 00:32:48 -0300

    :lipstick: Updating app theme

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,6 +1,7 @@
 import { colors } from "@theme-ui/preset-bootstrap";
 import { tailwind } from "@theme-ui/presets";
-import { borderRadius } from "styled-system";
+import { borderRadius, width } from "styled-system";
+// import myBackgroundImage from "../assets/svg/myBackgroundImage";
 
 const theme = {
   ...tailwind,
@@ -12,13 +13,28 @@ const theme = {
       background: "rgba(255, 255, 255, 0.1)",
       boxShadow: "20px 20px 50px rgba(0, 0, 0, 0.5)",
       borderRadius: "10px",
-      backdropFilter: "blur(5px)",
+      backdropFilter: "blur(2px)",
       color: tailwind.colors.gray[3],
       display: "flex",
       alignItems: "center",
       justifyContent: "space-between",
       textTransform: "uppercase",
       fontFamily: tailwind.fonts.sans,
+    },
+  },
+  containers: {
+    page: {
+      height: "100%",
+      width: "90%",
+      background: "yellow",
+    },
+    me: {
+      background: "yellow",
+      height: "300px",
+      with: "300px",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
     },
   },
 };
