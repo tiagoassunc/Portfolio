7e183934bbd3827697fd0b6df04e5173310027f4
Tiago Assunção <tiagoassunc@gmail.com>
2021-11-14 17:00:04 -0300

    :lipstick: Updating theme

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -8,37 +8,36 @@ const theme = {
   navigation: {
     NavHeader: {
       width: "90vw",
-      height: "60px",
+      maxHeight: "70px",
       padding: "0px 50px",
-      background: "rgba(255, 255, 255, 0.1)",
-      boxShadow: "20px 20px 50px rgba(0, 0, 0, 0.5)",
-      borderRadius: "10px",
-      backdropFilter: "blur(2px)",
       color: tailwind.colors.gray[3],
       display: "flex",
       alignItems: "center",
       justifyContent: "space-between",
       textTransform: "uppercase",
       fontFamily: tailwind.fonts.sans,
+      fontSize: "18px",
     },
   },
   containers: {
+    glass: {
+      color: tailwind.colors.gray[3],
+      background: "rgba(255, 255, 255, 0.1)",
+      boxShadow: "20px 20px 50px rgba(0, 0, 0, 0.5)",
+      borderRadius: "10px",
+      backdropFilter: "blur(2px)",
+    },
     page: {
       height: "100%",
       width: "90%",
-      background: "yellow",
     },
     me: {
-      background: "yellow",
-      height: "300px",
-      with: "300px",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "space-between",
+      border: "2px solid blue",
+      borderRadius: "50%",
     },
   },
 };
 
-console.log(theme);
+console.log(tailwind);
 
 export default theme;
