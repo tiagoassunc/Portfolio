455bda5daddcc1f562005fb0848414dc533add43
Tiago Assunção <tiagoassunc@gmail.com>
2021-11-18 19:15:30 -0300

    :lipstick: Updating UI modules and theme

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,7 +1,6 @@
 import { colors } from "@theme-ui/preset-bootstrap";
 import { tailwind } from "@theme-ui/presets";
-import { borderRadius, width } from "styled-system";
-// import myBackgroundImage from "../assets/svg/myBackgroundImage";
+import { borderRadius, padding, width } from "styled-system";
 
 const theme = {
   ...tailwind,
@@ -10,6 +9,7 @@ const theme = {
       width: "90vw",
       maxHeight: "70px",
       padding: "0px 50px",
+      // color: "black",
       color: tailwind.colors.gray[3],
       display: "flex",
       alignItems: "center",
@@ -18,26 +18,42 @@ const theme = {
       fontFamily: tailwind.fonts.sans,
       fontSize: "18px",
     },
+    navConfig: {},
   },
   containers: {
     glass: {
+      // color: "black",
       color: tailwind.colors.gray[3],
       background: "rgba(255, 255, 255, 0.1)",
       boxShadow: "20px 20px 50px rgba(0, 0, 0, 0.5)",
       borderRadius: "10px",
       backdropFilter: "blur(2px)",
     },
+    blackGlass: {
+      color: tailwind.colors.gray[3],
+      background: "rgba(0, 0, 0, 0.75)",
+      borderRadius: "10px",
+      backdropFilter: "blur(10px)",
+    },
     page: {
       height: "100%",
       width: "90%",
     },
-    me: {
-      border: "2px solid blue",
+    circle: {
       borderRadius: "50%",
+      border: "4px solid rgba(255, 255, 255, 0.25)",
+      boxShadow: "20px 20px 50px rgba(0, 0, 0, 0.5)",
+      overflow: "hidden",
+    },
+    card: {},
+  },
+  images: {
+    me: {
+      transform: `scale(${tailwind.transforms.scale["125"]}) translate(0, ${tailwind.transforms.translate["10"]})`,
     },
   },
 };
 
-console.log(tailwind);
+// console.log(tailwind);
 
 export default theme;
