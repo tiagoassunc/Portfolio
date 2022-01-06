f6c1494975a1ade9e81aa8c3d6165232af1d6f78
Tiago Assunção <tiagoassunc@gmail.com>
2022-01-05 22:42:12 -0300

    :wrench: Add and update vitals configurations

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testEnvironment: 'jsdom',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+};
