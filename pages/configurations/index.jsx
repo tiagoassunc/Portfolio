53937dd4a8b6d7f6dbcdf6446e528f90808e7c23
Tiago Assunção <tiagoassunc@gmail.com>
2022-01-09 12:39:21 -0300

    :sparkles:  Foundations of config

--- pages/configurations/index.jsx
+++ pages/configurations/index.jsx
@@ -1,17 +1,111 @@
-import React from "react";
-import { useRouter } from "next/router";
+/** @jsxImportSource theme-ui */
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { marginTop } from 'styled-system';
+import commons from './../../src/sass/commons.module.scss';
 
 const Config = () => {
+  const { t } = useTranslation();
+  const traslateble = t;
+
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        color: "#fff",
-      }}
-    >
-      <h1 style={{ fontSize: "100px" }}>🧱 Building... 🏗️</h1>
+    <div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          color: '#fff',
+          flexDirection: 'column'
+        }}
+      >
+        <h1 style={{ fontSize: '50px' }}>{traslateble('Languages')}</h1>
+        <div
+          sx={{
+            variant: 'general.position.centralize',
+            justifyContent: 'space-between',
+            maxWidth: '300px',
+            width: '100%'
+          }}
+        >
+          <button
+            className={commons.removeBtnStyle}
+            sx={{
+              variant: 'navigation.LangBtn',
+              backgroundColor: 'green'
+            }}
+          >
+            🇧🇷
+          </button>
+          <button
+            className={commons.removeBtnStyle}
+            sx={{
+              variant: 'navigation.LangBtn',
+              backgroundColor: 'blue'
+            }}
+          >
+            🇺🇸
+          </button>
+          <button
+            className={commons.removeBtnStyle}
+            sx={{
+              variant: 'navigation.LangBtn',
+              backgroundColor: 'red'
+            }}
+          >
+            🇯🇵
+          </button>
+        </div>
+      </div>
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          color: '#fff',
+          flexDirection: 'column',
+          marginTop: '35px'
+        }}
+      >
+        <h1 style={{ fontSize: '50px' }}>{traslateble('Themes')}</h1>
+        <div
+          sx={{
+            variant: 'general.position.centralize',
+            justifyContent: 'space-between',
+            maxWidth: '300px',
+            width: '100%'
+          }}
+        >
+          <button
+            className={commons.removeBtnStyle}
+            sx={{
+              variant: 'navigation.LangBtn',
+              backgroundColor: 'green'
+            }}
+          >
+            🎍
+          </button>
+          <button
+            className={commons.removeBtnStyle}
+            sx={{
+              variant: 'navigation.LangBtn',
+              backgroundColor: 'blue'
+            }}
+          >
+            ☃️
+          </button>
+          <button
+            className={commons.removeBtnStyle}
+            sx={{
+              variant: 'navigation.LangBtn',
+              backgroundColor: 'red'
+            }}
+          >
+            🧨
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
